9b49f6f311ea2b1d5ebb43b5a5ddfd24c21cb71e
Jamal <bluedocjamal@hotmail.co.uk>
2016-04-06 12:52:41 +0100

    Main class implemented with behaviors

---
+++ Main.java
@@ -0,0 +1,27 @@
+import lejos.robotics.subsumption.*;
+import lejos.nxt.*;
+
+/*
+ * Assignment 2 for Mobile Robotics 
+ * Implementing the Behavior API, allow the arbitrator to carry out behaviors
+ * depending on the conditions
+ * 
+ * @author Jamal Mahmoud - C13730921
+ * @author Minni Hiltunen - D15123113
+ */
+
+public class Main {
+	public static void main(String[] args) {
+		MapRoom map = new mapRoom();
+
+		Behavior obj = new ObjectDetect(map.getLength(), map.getWidth());//detects objects
+		Behavior surf = new SurfaceDetect();//detects surface type
+		Behavior end = new HitWall();//end condition
+		Behavior[] steps = {obj, surf, end};
+
+
+		Arbitrator controller = new Arbitrator(steps);
+
+		controller.start();
+	}
+}
