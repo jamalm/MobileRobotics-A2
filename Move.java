00379b142fc7c8b8e8302a2bd2514974a516835b
Jamal <bluedocjamal@hotmail.co.uk>
2016-04-06 13:46:51 +0100

    Added object detection

---
+++ Move.java
@@ -0,0 +1,36 @@
+import lejos.robotics.subsumption.*;
+
+import lejos.nxt.*;
+import lejos.robotics.navigation.DifferentialPilot;
+
+
+public class Move implements Behavior{
+	
+	private boolean suppressed = false;
+	private DifferentialPilot pilot;
+	private float travelled;	
+
+	
+	public Move(){
+		pilot = new DifferentialPilot(2.25f, 5.5f, Motor.A, Motor.B);
+	}
+
+	public void action(){
+		suppressed = false;
+		pilot.resetTachoCount();
+		pilot.forward();
+	}
+	
+	public void suppress(){
+		suppressed = true;
+		travelled = pilot.getMovement.getDistanceTraveled();
+	}
+
+	public boolean takeControl(){
+		return true;
+	}
+	
+	public float getPosition(){
+		return travelled;
+	}
+}
---
+++ ObjectDetect.java
@@ -0,0 +1,52 @@
+import lejos.robotics.subsumption.*;
+import lejos.nxt.*;
+import lejos.robotics.navigation.DifferentialPilot;
+
+
+/*
+
+*/
+
+public class ObjectDetect implements Behavior{
+	//fields
+	private boolean suppressed = false;
+	private float length, width;
+	private DifferentialPilot pilot;
+	private UltrasonicSensor sonar;
+	private Behavior position;
+
+	//constructor
+	public ObjectDetect(float _length, float _width){
+		sonar = new UltrasonciSensor(SensorPort.S4);
+		pilot = new DifferentialPilot(2.25f, 5.5f, Motor.A, Motor.B);
+		length = _length;
+		width = _width;
+		position = new Move();
+	}
+
+	public void action(){
+		suppressed = false;
+		
+		if(position < (length - (length/4))){
+			pilot.stop();
+			pilot.rotate(90);
+
+			pilot.steer(35, 90);
+
+			pilot.rotate(90);
+		}
+		else{
+			pilot.rotate(90);
+			pilot.travel(7);
+			pilot.rotate(90);
+		}
+	}
+
+	public void suppress(){
+		suppressed = true;
+	}
+
+	public boolean takeControl(){
+		return sonar.getDistance() < 35;
+	}
+}
